d28ae8cd66e604d851fc61365cf2457f37debfd8
yue leng <yue.leng@foxmail.com>
2019-06-13 01:05:10 +0800

    1

--- src/pokemon_searching.cpp
+++ src/pokemon_searching.cpp
@@ -171,17 +171,19 @@ public:
         cout << "x: " <<abs((r.br().x+r.tl().x)/2-320) << endl;
         cout << "y: " <<abs((r.tl().y+r.br().y)/2-240) << endl;
         cout << "width: " <<r.br().x-r.tl().x << endl;
-        float rate = (r.br().x-r.tl().x)/(r.br().y-r.tl().y)
+        float rate = (r.br().x-r.tl().x)/(r.br().y-r.tl().y);
         // cout << "y_center: " <<(r.tl().y+r.br().y)/2 << endl;
         // printf("width:%d height%f x_center:%f y_center:%f\n", r.br().x-r.tl().x, r.br().y-r.tl().y, (r.br().x+r.tl().x)/2,(r.tl().y+r.br().y)/2);
 		// printf("width:%d height%d\n", width, height);
 
+		if (rate<0.8) cout << rate<< endl;
+
 		if (abs((r.br().x+r.tl().x)/2-320)<150 
 			&& abs((r.tl().y+r.br().y))/2-240<150
-			&& r.br().x-r.tl().x > 70) 
-			&& rate < 0.8
+			&& r.br().x-r.tl().x > 70
+			&& rate < 0.8)
 		{	good = true;
-			cout << rate<< endl;
+			cout << "good:"<<rate<< endl;
 		}
 			
 		else good = false;
