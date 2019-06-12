d6e1f5c117dc488bed80f4c138ef10acb81a438c
MARKZXX\Administrator <mark.zxx@qq.com>
2019-06-12 22:33:54 +0800

    1

--- src/pokemon_searching.cpp
+++ src/pokemon_searching.cpp
@@ -65,9 +65,9 @@ public:
 	void collect_tag(const apriltags::AprilTagDetections &apriltags) {
 		for (auto atg : apriltags.detections) {
             tagMap[atg.id] = atg.pose;
-            double tmp = atg.secnod.orientation.x + atg.secnod.orientation.y + atg.secnod.orientation.z;
-            printf("id:%d x:%f y:%f z:%f w:%f sum:%f\n", atg.first, atg.secnod.orientation.x, atg.secnod.orientation.y,
-                   atg.secnod.orientation.z, tmp);
+            double tmp = atg.orientation.x + atg.orientation.y + atg.orientation.z;
+            printf("id:%d x:%f y:%f z:%f w:%f sum:%f\n", atg.first, atg.orientation.x, atg.orientation.y,
+                   atg.orientation.z, tmp);
 
 //			if(tagNumric[atg.id] != tmp)
 //            	autoSave();
